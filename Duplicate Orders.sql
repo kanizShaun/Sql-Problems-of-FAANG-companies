b55f6aee8885132a5cde73f499181ebca0c7921b
kanizShaun <87516448+kanizShaun@users.noreply.github.com>
2022-03-08 10:09:51 -0700

    Delete Duplicate Orders.sql
    initial commit

--- Duplicate Orders.sql
+++
@@ -1,2 +0,0 @@
-#Find customers who appear in the orders table more than three times. 
-select a.cust_id from (select cust_id, count(cust_id) as number_of_id from orders group by cust_id)a where number_of_id > 3
