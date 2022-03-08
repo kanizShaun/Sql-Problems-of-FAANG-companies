78113a4ec1a1c9e8c6ac7c98c182b9cec1337ff7
kanizShaun <87516448+kanizShaun@users.noreply.github.com>
2022-03-08 10:08:55 -0700

    Delete Active Users Per Platform.sql
    initial commit

--- Active Users Per Platform.sql
+++
@@ -1,5 +0,0 @@
-#For each platform (e.g. Windows, iPhone, iPad etc.), 
-#calculate the number of users. Consider unique users 
-#and not individual sessions. 
-#Output the name of the platform with the corresponding number of users. 
-select platform, count(user_id) as number_of_users from user_sessions group by platform order by count(user_id) desc
